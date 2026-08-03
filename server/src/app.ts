77a71eefa388752c397ee0d2b12b55b9c225e67d
SahilKayasth-16 <sahilkayasth1612@gmail.com>
2026-08-03 22:40:48 +0530

    Update app.ts with frontend new url

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,7 +16,7 @@ const app = express();
 //MIDDLEWARE
 app.use(
     cors({
-        origin: [ "http://localhost:5173", "https://insurance-management-platform-mu.vercel.app" ],
+        origin: [ "http://localhost:5173", "https://secure-shield-insurance-platform.vercel.app" ],
         credentials: true
     })
 );
